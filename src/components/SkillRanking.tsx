0e1b7740e401da11a10d2ba741a50cab3947450b
lanleimeng <ramon.silvanus@gmail.com>
2025-09-21 22:37:27 +0800

    delete unused import
    deleting unused imports

--- src/components/SkillRanking.tsx
+++ src/components/SkillRanking.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
-import { Progress } from './ui/progress';
+
 import { Badge } from './ui/badge';
 
 interface SkillRankingProps {
